4662194466f39a35a099a79fbec89746b1bad722
YuiYukihira <littlespec200000@gmail.com>
2018-03-21 01:42:00 +0000

    Docs: Added comments to the code to make it easier to understand.

--- core.py
+++ core.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 GRAPH_SHAPE = Dict[str, Dict[str, Union[str, List[Tuple[str, float, float]]]]]
 
 class Graph:
+    """A base graph, defines a shape and also creates and stores a tensorflow graph."""
     def __init__(self, shape: GRAPH_SHAPE):
         """
         shape: dict{
@@ -29,58 +30,62 @@ class Graph:
                 self.exit_nodes.append(key)
 
     def run(self, ins: Dict[str, Union[int, float]]) -> Tuple[List[float], List[str]]:
+        """Takes a dictionary of input node name, and value. Returns a tuple of a list of output values, and a list of output nodes."""
         logging.info('Compute Graph executed!')
-        fetches = [i+':0' for i in self.exit_nodes]
+        fetches = [i+':0' for i in self.exit_nodes] # Rename the node names so we get the tensorflow ID's (this only works if the nodes have unique names.)
         new_ins = {}
         for key in ins.keys():
-            new_ins[key+':0'] = ins[key]
+            new_ins[key+':0'] = ins[key] # Rename the node names so we get the tensorflow ID's (this only works if the nodes have unique names.)
 
         if fetches:
-            with tf.Session(graph=self.working) as sess:
-                return (sess.run(fetches, new_ins), self.exit_nodes)
+            with tf.Session(graph=self.working) as sess: # Create a temporary tensorflow session called sess which uses our graph.
+                return (sess.run(fetches, new_ins), self.exit_nodes) # Fetch and return the output.
 
     def create_picture(self):
+        """Writes a picute of the graph for TensorBoard to use."""
         with tf.Session(graph=self.working) as sess:
             writer = tf.summary.FileWriter('logs/', sess.graph)
             self.run({'a': 0.5, 'b': 0.4})
             writer.close()
 
     def create_working(self):
+        """Called internally, fills the Tensorflow Graph "working" with the data from "shape"."""
         logging.info('Creating workable compute graph!')
-        work_nodes = {}
+        work_nodes = {} # A dictionary for storing our previously computed nodes.
 
         def make_node(node):
+            """Recursive function for populating the graph with the information"""
             with self.working.as_default():
-                enter_nodes = set(i[0] for i in self.shape[node]['nodes'])
-                missing_nodes = enter_nodes - set(work_nodes.keys())
+                enter_nodes = set(i[0] for i in self.shape[node]['nodes']) # Find the nodes that enter the target node.
+                missing_nodes = enter_nodes - set(work_nodes.keys()) # Find the nodes not yet in work_nodes.
                 for i_node in missing_nodes:
-                    make_node(i_node)
-                if self.shape[node]['type'] in ['hidden', 'exit']:
+                    make_node(i_node) # Run this function on the missing nodes.
+                if self.shape[node]['type'] in ['hidden', 'exit']: # If the node is a 'hidden' or 'exit' node.
                     add_list = []
-                    if enter_nodes != set():
-                        for i_node in enter_nodes:
+                    if enter_nodes != set(): # Check that there nodes entering this node.
+                        for i_node in enter_nodes: # If there is, add a node into the graph with the previous node, the weight and bias.
                             for n in self.shape[node]['nodes']:
                                 if n[0] == i_node:
                                     weight = n[1]
                                     bias = n[2]
                                     break
                             add_list.append(
-                                (
+                                ((
                                     work_nodes[i_node]*tf.constant(
                                         weight, dtype=tf.float32
                                     )
                                 )+tf.constant(
                                     bias, dtype=tf.float32
-                                )
+                                ))
                             )
                     else:
-                        add_list.append(tf.constant(0.0, dtype=tf.float32))
-                    work_nodes[node] = tf.add_n(add_list, name=node)
-                elif self.shape[node]['type'] == 'enter':
-                    work_nodes[node] = tf.placeholder(tf.float32, name=node)
+                        add_list.append(tf.constant(0.0, dtype=tf.float32)) # If there isn't an input node. create a fake input that outputs zero as tensorflow requires a tensor to have an input.
+                    work_nodes[node] = tf.add_n(add_list, name=node) # Create a new tensor that sums all the nodes together and add it to work_nodes with the id as the key.
+                elif self.shape[node]['type'] == 'enter': # If the node if type 'enter'
+                    work_nodes[node] = tf.sigmoid(tf.placeholder(tf.float32, name=node)) # Create a new placeholder tensor and add it to work_nodes ith the id as the key.
         with self.working.as_default():
             for node in self.shape:
-                make_node(node)
+                make_node(node) # Make every node in the shape.
 
 class NodeGene:
     """
@@ -142,32 +147,36 @@ class NeatGraph:
     def __init__(self, genes: Genotype):
         logging.info('NEAT Graph created!')
         self.genotype = genes
-        self.phenotype = Graph(self.convert_genes_to_usable_format(self.genotype))
+        self.phenotype = Graph(self.convert_genes_to_usable_format(self.genotype)) # create our phenotype from our genes.
 
     @staticmethod
     def convert_genes_to_usable_format(genotype: Genotype) -> GRAPH_SHAPE:
-        usable = {}
-        for node in genotype.nodes:
+        """The information in the Genotype class is not in the correct format so we have to convert it for the graph class."""
+        usable = {} # Dictionary of previously computed nodes.
+        for node in genotype.nodes: # For every node in the genotype.
             logging.debug(f'node: {node}')
-            ins = []
-            for conn in genotype.conns.values():
+            ins = [] # List of nodes that input into this one.
+            for conn in genotype.conns.values(): # For all the values in the genotype connections.
                 logging.debug(f'\tconn: {conn}')
-                if conn.out_node == node.id:
-                    ins.append((conn.in_node, conn.weight if conn.enabled else 0, 0))
+                if conn.out_node == node.id: # Does this connection have the node as it's output node?
+                    ins.append((conn.in_node, conn.weight if conn.enabled else 0, 0)) # Add the input node into in's with the id and weight and a bias of 0 as we don't use them.
                     logging.debug(f'\t\tins: {ins}')
-            usable[node.id] = {'nodes': ins, 'type': node.type}
+            usable[node.id] = {'nodes': ins, 'type': node.type} # Add the input nodes to the useable dict with the node id as the key.
             logging.debug(f'\tusable: {usable}')
-        return usable
+        return usable # Return the usable genes.
 
     def recalculate_phenotype(self):
+        """Recalculate the phenotype if needed (it shouldn't be)."""
         self.phenotype = Graph(self.convert_genes_to_usable_format(self.genotype))
 
     def run(self, ins):
+        """Return the output from phenotype.run"""
         return self.phenotype.run(ins)
 
 
 
 class NeatController:
+    """Controlls the NEAT process."""
     def __init__(self, genera: int, species: int, required_nodes: Dict[str, str]):
         self.genera_count = genera
         self.species_count = species
@@ -185,9 +194,11 @@ class NeatController:
                 self.scores[i][j] = 0
 
     def run(self, inputs):
+        """Return the output of the current graph's run method."""
         return self.graphs[self.current[0]][self.current[1]].run(inputs)
 
     def game_over(self, score):
+        """assign score to the current graph and move onto the next one. If no graphs left, call the breed method."""
         self.scores[self.current[0]][self.current[1]] = score
         if self.current == (self.genera_count - 1, self.species_count - 1):
             self.current = (0, 0)
@@ -203,28 +214,30 @@ class NeatController:
         new_graphs = {}
         child_counter = 0
 
-        for i in range(0, 5):
+        for i in range(0, 5): # Find the top 5 scoring graphs in the genera.
             top = max(graphs_copy[genera].keys(), key=(lambda key: graphs_copy[genera][key]))
             top5[top] = (self.graphs[genera][top], self.scores[genera][top])
             del graphs_copy[genera][top]
-        for a_tuple in top5.values():
+        for a_tuple in top5.values(): # Repeat for each graph in the top 5.
+            # Make Graph A.
             a_graph = a_tuple[0]
             a_score = a_tuple[1]
-            for b_tuple in top5.values():
+            for b_tuple in top5.values(): # Repeat for each graph in the top 5.
+                # Make the Graph B.
                 b_graph = b_tuple[0]
                 b_score = b_tuple[1]
-                if a_graph != b_graph:
+                if a_graph != b_graph: # Only breed if the two graphs are different.
                     a_genotype = b_graph.genotype
                     b_genotype = b_graph.genotype
-                    if a_score > b_score:
+                    if a_score > b_score: # Get the child's mutation rate from the most fit parent. If they are the same fitness, select randomly.
                         mutation_rate = a_genotype.mutation_rate
                     elif b_score > a_score:
                         mutation_rate = b_genotype.mutation_rate
                     else:
                         mutation_rate = random.choice([b_genotype.mutation_rate, a_genotype.mutation_rate])
-                    c_genotype = Genotype([], {}, mutation_rate)
-                    needed_nodes = set(self.required_nodes.keys())
-                    for innov_counter in range(0, self.global_innov):
+                    c_genotype = Genotype([], {}, mutation_rate) # Create and empty genotype C for the child.
+                    needed_nodes = set(self.required_nodes.keys()) # Find the nodes that are required as a bare minimum and add them to the set of needed nodes.
+                    for innov_counter in range(0, self.global_innov): # Repeat until we reach the global innovation score counter
                         a_gene = a_genotype.conns.get(innov_counter)
                         b_gene = b_genotype.conns.get(innov_counter)
                         if a_gene and b_gene:
@@ -250,26 +263,27 @@ class NeatController:
                             # gene not present in a, so inherit from b.
                             needed_nodes |= {b_gene.in_node, b_gene.out_node}
                             c_genotype.conns[innov_counter] = genera
-                    for node_id, node_type in self.required_nodes.items():
+                    for node_id, node_type in self.required_nodes.items(): # Add the node genes with information if they are from the required nodes.
                         if node_id in needed_nodes:
                             c_genotype.nodes.append(NodeGene(node_id, node_type))
                             needed_nodes -= {node_id}
-                    if needed_nodes:
-                        for node in a_genotype.nodes:
+                    if needed_nodes: # If there are still nodes left.
+                        for node in a_genotype.nodes: # Add the node genes with information from genotype A.
                             if node.id in needed_nodes:
                                 c_genotype.nodes.append(node)
                                 needed_nodes -= {node.id}
-                    if needed_nodes:
-                        for node in b_genotype.nodes:
+                    if needed_nodes: # If there are still nodes left.
+                        for node in b_genotype.nodes: # Add the node genes with information from genotype B.
                             if node.id in needed_nodes:
                                 c_genotype.nodes.append(node)
                                 needed_nodes -= {node.id}
-                    if random.randint(0, 100) < c_genotype.mutation_rate:
+                    if random.randint(0, 100) < c_genotype.mutation_rate: # Mutate the C genotype if it passes the check.
                         choice = random.randint(0,4)
                         if choice == 0:
                             # Change mutation rate
                             c_genotype.mutation_rate = random.randint(0,100)
                         elif choice == 1:
+                            # Add a new node.
                             if c_genotype.conns != {}:
                                 conn = random.choice(list(c_genotype.conns.keys()))
                                 c_genotype.conns[conn].enabled = False
@@ -281,6 +295,7 @@ class NeatController:
                                 c_genotype.nodes.append(NodeGene(node_id, "hidden"))
                                 self.global_innov += 2
                         elif choice == 2:
+                            # Add a new connection
                             c_shape = NeatGraph.convert_genes_to_usable_format(c_genotype)
                             #out_node = random.choice(list(c_shape.keys()))
                             possible_keys = set(c_shape.keys())
@@ -323,15 +338,13 @@ class NeatController:
                                 sign = random.randint(1,2)
                                 amount = random.random()*2
                                 c_genotype.conns[conn].weight = amount if sign == 1 else -amount
-                    new_graphs[child_counter] = NeatGraph(c_genotype)
+                    new_graphs[child_counter] = NeatGraph(c_genotype) # Make a NeatGraph from the genotype and add it test the list.
                     child_counter += 1
-                    if child_counter == self.species_count:
-                        return new_graphs
+                    if child_counter == self.species_count: # If we've hit the amount of children needed.
+                        return new_graphs # return the new graphs for this genera.
     def breed(self):
         new_graphs = {}
-        i = 0
-        for genera in self.graphs:
-            i += 1
-            n = self._process_genera(genera)
-            new_graphs[genera] = n
-        self.graphs = new_graphs
+        for genera in self.graphs: # For every genera.
+            n = self._process_genera(genera) # Get the new graphs for this genera.
+            new_graphs[genera] = n # Set the genera to the new values
+        self.graphs = new_graphs # replace the graphs with the new ones.
