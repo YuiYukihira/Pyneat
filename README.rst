75a522f0458fc0038836aa7c1500e6c4ac9c77a9
YuiYukihira <littlespec200000@gmail.com>
2018-03-26 23:02:09 +0100

    Add: Package is ready for pypi now.

---
+++ README.rst
@@ -0,0 +1,19 @@
+PyNeat
+======
+
+.. _tensorflow: https://www.tensorflow.org/
+
+An implementation of Kenneth O. Stanley's NEAT algorithm using tensorflow_ for the back end.
+
+Installation
+------------
+
+.. code:: sh
+
+  pip install -U pyneat
+
+
+Usage Example
+-------------
+
+.. literalinclude:: pyneat/examples/pole.py
--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
 setup(name="pyneat",
       version="0.1",
       description="An implementation of Kenneth O. Stanley's NeuroEvolution of Augmenting Topologies",
+      long_description=readme,
       url="https://gitlab.com/NeatJumper/Neat",
       author="Yui Yukihira",
       author_email="yui.yukihira42@gmail.com",
       license="MIT",
       packages=['pyneat'],
-      install_requires=[
-          'tensorflow'],
+      install_requires=requirements,
+      python_requires=">=3.6.4",
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3 :: Only',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ])
